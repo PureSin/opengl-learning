5013697157cff0c244137f1e56886f5f4195ba78
Kelvin Ma <kelvin@kelvin.ma>
2021-04-06 19:23:01 -0700

    Tutorial 6: enable mouse and keyboard to change the matrix

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -16,6 +16,7 @@ using namespace glm;
 
 #include <common/shader.hpp>
 #include <common/texture.hpp>
+#include <common/controls.hpp>
 
 int main( void )
 {
@@ -55,6 +56,13 @@ int main( void )
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+        // Hide the mouse and enable unlimited mouvement^M
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+       // Set the mouse at the center of the screen^M
+      glfwPollEvents();
+     glfwSetCursorPos(window, 1024/2, 768/2);
+    
     // Dark blue background
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
@@ -62,26 +70,11 @@ int main( void )
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
-    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    int width = 1024;
-    int height = 768;
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float)height, 0.1f, 100.0f);
-      
-    // Camera matrix
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
-        glm::vec3(0,0,0), // and looks at the origin
-        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-        );
-    
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 Model = glm::mat4(1.0f);// Our ModelViewProjection : multiplication of our 3 matrices
-    glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
-    
     // Create and compile our GLSL program from the shaders
     GLuint programID = LoadShaders("VertexShader.vsh", "FragmentShader.fsh" );
 
-    GLuint Texture = loadBMP_custom("uvtemplate.bmp");
+    GLuint Texture = loadDDS("uvtemplate.DDS");
+    //GLuint Texture = loadBMP_custom("uvtemplate.bmp");
     
     // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
     // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
@@ -186,6 +179,17 @@ int main( void )
         // Use our shader
         glUseProgram(programID);
 
+        // Compute the MVP matrix from keyboard and mouse input
+           computeMatricesFromInputs();
+           glm::mat4 ProjectionMatrix = getProjectionMatrix();
+           glm::mat4 ViewMatrix = getViewMatrix();
+           glm::mat4 ModelMatrix = glm::mat4(1.0);
+           glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+        
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        
         // 1st attribute buffer : vertices
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -210,9 +214,7 @@ int main( void )
             (void*)0                          // array buffer offset
         );
 
-        // Send our transformation to the currently bound shader, in the "MVP" uniform
-        // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
         
         // Enable depth test
         glEnable(GL_DEPTH_TEST);
